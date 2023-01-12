b3bf7c3d2c803c34cd3bdc865cb9d54186eafd6f
Mt-Lampert <perrin.blog@web.de>
2023-01-12 15:22:30 +0100

    w: outlined query form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1 class="is-size-1 has-text-danger">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+</script>
+
+<main>
+	<div class="form-container">
+		<h1 class="title">Welcome</h1>
+		<div class="form-top">
+			<div>
+				<input class="input" type="text" placeholder="Your ingredient" />
+			</div>
+			<div>
+				<select class="select" id="diet" name="diet">
+					<option>vegetarian</option>
+					<option>vegan</option>
+					<option>low carb</option>
+					<option>high protein</option>
+				</select>
+			</div>
+		</div>
+		<div class="form-submit"><button class="button is-warning">Submit</button></div>
+	</div>
+</main>
+
+<style lang="scss">
+	main {
+		margin: 2rem auto 0rem auto;
+		width: 800px;
+	}
+
+	.form-container {
+		background-color: hsl(140, 60%, 50%);
+		width: 600px;
+		padding: 2rem 3rem;
+	}
+</style>
